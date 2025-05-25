9ddb487271fedbce64120263c8815c01693b5846
htt7604 <112330899+htt7604@users.noreply.github.com>
2025-05-25 08:19:23 +0700

    Add files via upload

---
+++ QL_Nha-thuoc/QL_Nha-thuoc/FormMain.Designer.cs
@@ -0,0 +1,56 @@
+﻿namespace QL_Nha_thuoc
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            panel1 = new Panel();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.Location = new Point(0, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(200, 720);
+            panel1.TabIndex = 0;
+            // 
+            // FormMain
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1262, 721);
+            Controls.Add(panel1);
+            Name = "FormMain";
+            Text = "FormMain";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel panel1;
+    }
+}
